6ffe6d664482fdc6d415e88f68d607f71e0a5fbb
makoto <makoto09142425@gmail.com>
2020-08-17 12:11:51 +0900

    /user/new表示画面設定

--- hello_project/urls.py
+++ hello_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
+    path('user/', include('user.urls')),
+    # path('myapp/', include('myapp.urls')),
 ]
