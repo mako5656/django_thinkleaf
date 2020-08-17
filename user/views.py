79a7f6bc00470c61fb5aee4b265e5263ea60f82d
makoto <makoto09142425@gmail.com>
2020-08-17 12:30:22 +0900

    フォーム画面作成

--- user/views.py
+++ user/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .forms import UserForm
+
 
 def new(request):
-    params = {'message': 'newです'}
+    params = {'name': '', 'email': '', 'form': None}
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        params['name'] = request.POST['name']
+        params['email'] = request.POST['email']
+        params['form'] = form
+    else:
+        params['form'] = UserForm()
     return render(request, 'user/new.html', params)
